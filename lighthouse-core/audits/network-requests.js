7cc759768b2205c1edb8e91c96e7f5adf995d7a1
Patrick Hulce <patrick.hulce@gmail.com>
2018-07-13 09:58:27 -0700

    core: warn when extensions affected perf

--- lighthouse-core/audits/network-requests.js
+++ lighthouse-core/audits/network-requests.js
@@ -18,7 +18,7 @@ class NetworkRequests extends Audit {
       scoreDisplayMode: Audit.SCORING_MODES.INFORMATIVE,
       title: 'Network Requests',
       description: 'Lists the network requests that were made during page load.',
-      requiredArtifacts: ['devtoolsLogs'],
+      requiredArtifacts: ['devtoolsLogs', 'LighthouseRunWarnings'],
     };
   }
 
@@ -27,6 +27,7 @@ class NetworkRequests extends Audit {
    * @return {Promise<LH.Audit.Product>}
    */
   static audit(artifacts) {
+    const runWarnings = artifacts.LighthouseRunWarnings || [];
     const devtoolsLog = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
     return artifacts.requestNetworkRecords(devtoolsLog).then(records => {
       const earliestStartTime = records.reduce(
@@ -49,6 +50,11 @@ class NetworkRequests extends Audit {
           resourceType: record.resourceType,
         };
       });
+
+      if (results.some(result => result.url.startsWith('chrome-extension'))) {
+        runWarnings.push('Chrome extensions impacted the performance of this page. ' +
+          'Try running Lighthouse in a clean profile to avoid being impacted by extensions.');
+      }
 
       const headings = [
         {key: 'url', itemType: 'url', text: 'URL'},
